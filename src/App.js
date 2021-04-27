a5b4ce56b1dfc9112965bb26b9fae501cf0017b4
daviddaville <daviddaville@gmail.com>
2021-04-27 21:17:35 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
-import logo from './logo.svg';
+import Page from './components/Page';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
       <Router>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <div>
-                <h1>Welcome</h1>
-              </div>
-            )}
-          />
-          <Route
-            path="/dashboard"
-            render={() => (
-              <div>
-                <h1>Dashboard</h1>
-                <Link to="/" id="click-me">
-                  Home
-          </Link>
-              </div>
-            )}
-          />
-        </Switch>
+        <div className="App">
+          <Switch>
+            <Route path="/" component={Page} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
